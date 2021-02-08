1f09edd8b7fbc6e9deb36d16614d8839f1f9af90
Yarchik <yarchike@gmail.com>
2021-02-08 20:26:00 +0300

    Сделал экран контактов

--- app/src/main/java/com/martynov/frontcovid/ui/ContacktActivity.kt
+++ app/src/main/java/com/martynov/frontcovid/ui/ContacktActivity.kt
@@ -2,11 +2,36 @@ package com.martynov.frontcovid.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.widget.Toolbar
 import com.martynov.frontcovid.R
+import kotlinx.android.synthetic.main.activity_contackt.*
 
 class ContacktActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contackt)
+        setSupportActionBar(findViewById<Toolbar>(R.id.toolbar))
+        supportActionBar!!.setSubtitle(getString(R.string.contacts))
+        val clickListener = View.OnClickListener {
+            when(it.id){
+                R.id.btnWork ->{
+
+                }
+                R.id.btnFamily ->{
+
+                }
+                R.id.btnFriends ->{
+
+                }
+                R.id.btnOther -> {
+
+                }
+            }
+        }
+        btnWork.setOnClickListener(clickListener)
+        btnFamily.setOnClickListener(clickListener)
+        btnFriends.setOnClickListener(clickListener)
+        btnOther.setOnClickListener(clickListener)
     }
 }
